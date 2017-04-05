0340b0761cb2910545920687dcebab5fbcb9e9a5
13werwolf13 <13werwolf13@mail.ru>
2017-04-05 12:37:32 +0600

    снёс нафиг лишнее

--- calculator.py
+++ calculator.py
@@ -6,8 +6,6 @@ class Calculator(object):
     def help(self):
         return "\nиспользование 'действие' 'число 1' 'число 2'\n\n minus - вычесть второе число из первого\n plus - сложить два числа\n multiply - перемножить два числа\n divide - разделить первое число на второе\n"
 
-    def double(self, number):
-        return 2 * number
     def minus(self, number1, number2):
         return number1 - number2
     def plus(self, number1, number2):
